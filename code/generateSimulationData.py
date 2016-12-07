e7b61d774599f2dd08d85de8e729197bde61ca31
Jenna Schabdach <jmschabdach@gmail.com>
2016-12-06 19:17:23 -0500

    ok I'm tired

--- code/generateSimulationData.py
+++ code/generateSimulationData.py
@@ -657,7 +657,7 @@ elif args.runtype == 1:
         if yRound > 0.0: 
             abnormals = np.asarray[generateAbnormalNode(zerosImgs, onesImgs, model) for j in xrange(yRound)]
             abnormalFeats = np.asarray([row[0] for row in abnormals])
-            abnormalImgs = np.asarray[row[1] for row in abnormals])
+            abnormalImgs = np.asarray([row[1] for row in abnormals])
             # add the generated features to the list for that patient
             patFeats[i] = np.concatenate((normalFeats, abnormalFeats))
             # add the generated images to the list for that patient
