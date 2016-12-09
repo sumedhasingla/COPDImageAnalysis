1a4b4f01cf94360386745c98a4493dee6e362cd6
Jenna Schabdach <jmschabdach@gmail.com>
2016-12-09 08:48:14 -0500

    code updates from 1 am

--- code/interpretSubjectGraph.py
+++ code/interpretSubjectGraph.py
@@ -8,6 +8,9 @@ from argparse import RawTextHelpFormatter
 
 import numpy as np
 import cPickle as pickle
+import pickle as pk
+
+# from plot_ave_roc import *
 
 """
 Notes:
@@ -98,81 +101,66 @@ def loadKernel(fn):
     print("Kernel loaded!")
     return loader["similarities"]
 
+def saveCoeffs(fn, coeffs):
+    """
+    Save the coefficients (nus) to a hdf5 file.
+
+    Inputs:
+    - fn: filename to save to
+    - coeffs: nus
+
+    Returns: nothing
+    """
+    with h5py.File(fn,'w') as f:
+        ds = f.create_dataset("coeff", coef.shape, dtype=coef.dtype, compression='gzip', compression_opts=9, data=coef)
+
+def loadCoeffs(fn):
+    """
+    Load the coefficients from a file.
+
+    Inputs:
+    - fn: filename to read from
+
+    Returns:
+    - coeffs: the loaded coefficients
+    """
+    with h5py.File(fn, 'r') as hf:
+        coeffs = np.array(hf.get("coeff"))
+    return coeffs
+
+def saveInterpModel(fn, model):
+    """
+    Save the model used for the interpolation into a pickle file
+
+    Inputs:
+    - fn: filename
+    - model: the model
+
+    Returns: nothing
+    """
+    fid = open(fn,'wb')
+    pickle.dump(model, fid, pickle.HIGHEST_PROTOCOL )
+    fid.close()
+
+def loadInterpModel(fn):
+    """
+    Load the pickle file containing the model.
+
+    Inputs:
+    - fn: filename to load from
+
+    Returns:
+    - model: the interpolation model loaded from the pickle file
+    """
+    with open(fn, "rb") as f:
+        model = pk.load(f)
+    return model
+
 #-----------------------------------------------------------------------------------------
 # Interpretation functions
 #-----------------------------------------------------------------------------------------
 
-def interpretSubject(targets, edgeMatrix, numNodesList, simWeight):
-    """
-    This function interpret given y locally for a subject 
-
-    Inputs:
-    - targets:
-    - edgeMatrix:
-    - numNodesList:
-    - simWeight: weights for subject i (row of subj i from kernel)
-
-    Returns
-    - coef: nu (weights)
-    - clf: name of the model
-    """
-    from scipy.sparse import csr_matrix
-    #from sklearn.feature_selection import SelectFromModel
-    #from sklearn.linear_model import LassoCV
-    #from sklearn.linear_model import LassoLarsCV
-    from sklearn.linear_model import LarsCV
-
-
-    # first make a binary matrix the same size as edgeMatrix, it has one if there is knn edge
-    # find the node farthest away from the each node for each block
-    rows = np.array( (edgeMatrix - (edgeMatrix.max(axis=0)).todense()).argmax(axis=0)  ).squeeze()
-    cols = np.array(range( len(rows) ))
-    data = np.ones(len(cols),)
-    knnMatrix =  csr_matrix( (data,(rows,cols)), shape=edgeMatrix.shape )
-    knnMatrix = knnMatrix.todense()
-
-    print(knnMatrix.shape)
-    
-    # Make a popularity vector for each subject
-    startIndex = 0
-    popularityVec = []
-    for n in numNodesList: # all 400
-        popularityVec.append( np.array(knnMatrix[:,startIndex:(startIndex+n)].sum(axis=1)) )
-        startIndex += n
-        
-    # make the popularity feature and normalize it    
-    popularityVec = np.hstack(popularityVec)    
-    # normalize vector -- similar to idf idea
-    popularityVecMean = popularityVec.mean(axis=1)
-    popularityVecMean[popularityVecMean==0.0] = 1.0   # make sure there is no devision by zero
-    popularityVecNormalized = popularityVec/popularityVecMean[:,np.newaxis]
-    
-    
-    # approximate the target using sparse linear model
-    X = popularityVecNormalized.T
-    # bringing the important weight inside of the loss function b/c it is not implemented in 
-    X2 = np.sqrt(simWeight[:,np.newaxis])*X 
-    y2 = np.sqrt(simWeight)*targets
-
-    #clf = LassoCV(cv=10,n_jobs=-1)
-    #clf = LassoLarsCV(cv=10,n_jobs=-1)
-    clf = LarsCV(cv=10, normalize=False)
-    #sfm = SelectFromModel(clf, threshold=0.25)
-    #sfm.fit(X2, y2)
-    #n_features = sfm.transform(X2).shape[1]
-    #if not(maxNumRegions==None):
-    #    while n_features > maxNumRegions:
-    #        sfm.threshold += 0.1
-    #        n_features = sfm.transform(X2).shape[1]
-    #        
-    #print("number of features : ", n_features)
-    #indices = sfm.get_support(indices=True)
-    #coef = sfm.get_params()
-    clf.fit(X2, y2)
-    coef = clf.coef_
-    return coef, clf 
-   
-def createH5File(sid, subjList, sample_weights, predictions, fnRoot):
+def createPreProcFile(sid, subjList, sample_weights, predictions, fnRoot):
     """
     Create a compiled H5 file
 
@@ -222,54 +210,163 @@ def createH5File(sid, subjList, sample_weights, predictions, fnRoot):
 
     f.close()
 
+def interpretSubject(targets, edgeMatrix, numNodesList, simWeight):
+    """
+    This function interpret given y locally for a subject 
+
+    Inputs:
+    - targets:
+    - edgeMatrix:
+    - numNodesList:
+    - simWeight: weights for subject i (row of subj i from kernel)
+
+    Returns
+    - coef: nu (weights)
+    - clf: name of the model
+    """
+    from scipy.sparse import csr_matrix
+    #from sklearn.feature_selection import SelectFromModel
+    #from sklearn.linear_model import LassoCV
+    #from sklearn.linear_model import LassoLarsCV
+    from sklearn.linear_model import LarsCV
+
+
+    # first make a binary matrix the same size as edgeMatrix, it has one if there is knn edge
+    # find the node farthest away from the each node for each block
+    rows = np.array( (edgeMatrix - (edgeMatrix.max(axis=0)).todense()).argmax(axis=0)  ).squeeze()
+    cols = np.array(range( len(rows) ))
+    data = np.ones(len(cols),)
+    knnMatrix =  csr_matrix( (data,(rows,cols)), shape=edgeMatrix.shape )
+    knnMatrix = knnMatrix.todense()
+
+    # print(knnMatrix.shape)
+    
+    # Make a popularity vector for each subject
+    startIndex = 0
+    popularityVec = []
+    for n in numNodesList: # all 400
+        popularityVec.append( np.array(knnMatrix[:,startIndex:(startIndex+n)].sum(axis=1)) )
+        startIndex += n
+        
+    # make the popularity feature and normalize it    
+    popularityVec = np.hstack(popularityVec)    
+    # normalize vector -- similar to idf idea
+    popularityVecMean = popularityVec.mean(axis=1)
+    popularityVecMean[popularityVecMean==0.0] = 1.0   # make sure there is no devision by zero
+    popularityVecNormalized = popularityVec/popularityVecMean[:,np.newaxis]
+    
+    
+    # approximate the target using sparse linear model
+    X = popularityVecNormalized.T
+    # print("simweight: ", simWeight.shape)
+    # print("shape of X: ", X.shape)
+    # bringing the important weight inside of the loss function b/c it is not implemented in 
+    X2 = np.sqrt(simWeight[:,np.newaxis])*X 
+    y2 = np.sqrt(simWeight)*targets
+
+    #clf = LassoCV(cv=10,n_jobs=-1)
+    #clf = LassoLarsCV(cv=10,n_jobs=-1)
+    clf = LarsCV(cv=10, normalize=False)
+    #sfm = SelectFromModel(clf, threshold=0.25)
+    #sfm.fit(X2, y2)
+    #n_features = sfm.transform(X2).shape[1]
+    #if not(maxNumRegions==None):
+    #    while n_features > maxNumRegions:
+    #        sfm.threshold += 0.1
+    #        n_features = sfm.transform(X2).shape[1]
+    #        
+    #print("number of features : ", n_features)
+    #indices = sfm.get_support(indices=True)
+    #coef = sfm.get_params()
+    clf.fit(X2, y2)
+    coef = clf.coef_
+    return coef, clf 
+   
 
 
 if __name__ == '__main__':
     # Set up argument parser
     parser = argparse.ArgumentParser(description=""" Subject Graph interpretor. """, formatter_class=RawTextHelpFormatter ) 
 
-    sparseFileRoot = "./simulatedData/sparseGraphs/"
+    # sparseFileRoot = "./simulatedData/sparseGraphs/"
     # sparseFileRoot = "/pylon2/ms4s88p/jms565/simulatedData/sparseGraphs/"
-    # parser.add_argument('--sparseFile', type=str, help='input sparse graph file', default=sparseFileRoot)
-    preProcFileRoot = "./simulatedData/interpretation/preProc_"
-    # preProcFileRoot = "/pylon2/ms4s88p/jsm565/simulatedData/interpretation/preProc_"
-    # parser.add_argument('--inputFile', type=str, help='input HDF5 file', required=False, default=preProcFileRoot)
-    coefFileRoot = "./simulatedData/interpData/coeffs/"
+    # parser.add_argument('--sparseFileRoot', type=str, help='input sparse graph file', required=True, default=sparseFileRoot)
+    # preProcFileRoot = "./simulatedData/interpretation/preProc/"
+    # preProcFileRoot = "/pylon2/ms4s88p/jms565/simulatedData/interpretation/preProc_"
+    # preProcFileRoot = os.environ['LOCAL']+"pre-"
+    # parser.add_argument('--preProcFileRoot', type=str, help='input HDF5 file', required=True, default=preProcFileRoot)
+    # coefFileRoot = "./simulatedData/interpretation/coeffs/"
     # coefFileRoot = "/pylon2/ms4s88p/jms565/simulatedData/interpData/coeffs/"
-    # parser.add_argument('--outCoefFile', type=str, help='output HDF5 file for coefficients', required=False, default=coefFileRoot)
-    pickleFileRoot = "./simulatedData/interpData/coeffs/"
-    # pickleFileRoot = "/pylon2/ms4s88p/jms565/simulatedData/interpData/coeffs/"
-    # parser.add_argument('--outPickFile', type=str, help='output Pickle file for LARS object', required=False, default=pickleFileRoot)
+    # coefFileRoot = os.environ['LOCAL']+"coeff-"
+    # parser.add_argument('--outCoefFileRoot', type=str, help='output HDF5 file for coefficients', required=True, default=coefFileRoot)
+    # pickleFileRoot = "./simulatedData/interpretation/models/"
+    # pickleFileRoot = "/pylon2/ms4s88p/jms565/simulatedData/interpData/models/"
+    # pickleFileRoot = os.environ['LOCAL']+"model-"
+    # parser.add_argument('--outPickFile', type=str, help='output Pickle file for LARS object', required=True, default=pickleFileRoot)
     runHelp = "Select which functions to run in the code:"
     runHelp += "    - 0: set up the h5py files containing info for a single subject's interpretation"
     runHelp += "    - 1: run the interpretation code"
-    runHelp += "    - 2: visualization of the interpretation"
-    parser.add_argument('-r', '--runtype', type=int, help=runHelp, default=0)
+    runHelp += "    - 2: processing of the interpretation (ROC)"
+    parser.add_argument('-r', '--runtype', type=int, help=runHelp, default=1)
     parser.add_argument('-s', '--subjectId', type=str, help='id of subject', default="S0000")
 
     args = vars(parser.parse_args())
-    # print( args )
+    print( args )
+    # print(args['sid'])
+    # sparseFileRoot = args['sparseFileRoot']
+    # preProcFileRoot = args['preProcFileRoot']
+    # coefFileRoot = args['outCoefFileRoot']
+    # pickleFileRoot = args['outPickleFileRoot']
+
+    # filepaths for local
+    sparseFileRoot = "./simulatedData/sparseGraphs/"
+    preProcFileRoot = "./simulatedData/interpretation/preProc/"
+    coefFileRoot = "./simulatedData/interpretation/coeffs/"
+    pickleFileRoot = "./simulatedData/interpretation/models/"
+
+    # filepaths for cluster
+    # sparseFileRoot = "./sparseGraphs/"
+    # preProcFileRoot = "./interpretation/preProc/"
+    # coefFileRoot = "./interpretation/coeffs/"
+    # pickleFileRoot = "./interpretation/models/"
 
     from scipy.sparse import csr_matrix
     import h5py
 
     if args['runtype'] == 0:
         # generate the files
-
-        # TESTING
-        sid = 'S0000'
         subjList = ['S'+str(i).zfill(4) for i in xrange(2000)]
-        kernelFN = "./simulatedData/kernel-kl"
-        kernelWeights = loadKernel(kernelFN)
-        predictionsFN = "./simulatedData/interpretation/predictions.hdf5"
-        predictions = loadPredictions(predictionsFN)
-        sparseFileRoot = "./simulatedData/interpretation/preProc_"
-        createH5File(sid, subjList, kernelWeights, predictions, sparseFileRoot)
+
+        for i in xrange(2000):
+            # sid = args['subjectId']
+            sid = "S" + str(i).zfill(4)
+            kernelFN = "./simulatedData/kernel-kl"
+            # kernelFN = "/pylon1/ms4s88p/jms565/simulatedData/kernel-kl"
+            kernelWeights = loadKernel(kernelFN)[i]
+            # print("Checking the shape of the kernel weights: ", kernelWeights.shape)
+            predictionsFN = "./simulatedData/interpretation/predictions.hdf5"
+            # predictionsFN = "/pylon2/ms4s88p/jms565/simulatedData/predictions.hdf5"
+            predictions = loadPredictions(predictionsFN)
+            createPreProcFile(sid, subjList, kernelWeights, predictions, preProcFileRoot)
+        print("Finished building the preprocessing files for all subjects!")
+
+        # to test and make sure files aren't the same
+        fn0 = "./simulatedData/interpretation/preProc/S0000.hdf5"
+        fn1 = "./simulatedData/interpretation/preProc/S0001.hdf5"
+        f0 = h5py.File(fn0, "r")
+        f1 = h5py.File(fn1, "r")
+        print("Checking some files...")
+        print("sample weights (false): ", (f1['/sample_weights'].value==f0['/sample_weights'].value).all())
+        print("predictions (true): ", (f1['/predicted'].value==f0['/predicted'].value).all())
+        f0.close()
+        f1.close()
+
 
     elif args['runtype'] == 1: 
         f = h5py.File(preProcFileRoot+args['subjectId']+'.hdf5', "r")
         # read environment variables 
-        sid = f['/sid'].value #subject id
+        # sid = f['/sid'].value #subject id
+        sid = args['subjectId']
         # knnGraphsRoot = f['/knnGraphsRoot'].value # not needed if provided as an input (--inputFile)
         # knnGraphsRoot = args['sparseFile']+sid
         knnGraphsRoot = sparseFileRoot+sid
@@ -283,33 +380,27 @@ if __name__ == '__main__':
         print( "working on: ",sid)
         EdgeMatrix_clean = loadSparseGraph(knnGraphsRoot+'.npz')
         numNodesList_clean = [400 for i in xrange(2000)]  # mal - hardcoding
+        # need to both be (2000,)
+        # print(predicted.shape)
+        # print(sample_weights.shape)
 
-        coef, clf = interpretSubject(predicted, EdgeMatrix_clean, 
-                         numNodesList_clean, sample_weights)
+        coef, clf = interpretSubject(predicted, EdgeMatrix_clean, numNodesList_clean, sample_weights)
 
         # saving the results
-        f = h5py.File(args['outCoefFile'],'w')
-        #hdfPath = '/idx'
-        #dset = f.create_dataset(hdfPath, idx.shape, 
-        #                         dtype=idx.dtype)
-        #dset[...] = idx
+        saveCoeffs(coefFileRoot+sid+".hdf5", coef)
+        saveInterpModel(pickleFileRoot+sid+".p.data", clf)
 
-        hdfPath = '/coef'
-        dset = f.create_dataset(hdfPath, coef.shape, 
-                                dtype=coef.dtype)
-        dset[...] = coef
+        # checking
+        # cl = loadCoeffs(coefFileRoot+sid+".hdf5")        
+        # ml = loadInterpModel(pickleFileRoot+sid+".p.data")
 
-        f.close()
-
-        fid = open(args['outPickFile'],'wb')
-        pickle.dump(clf, fid, pickle.HIGHEST_PROTOCOL )
-        fid.close()
+        # print("Checking save/load...")
+        # print("  coeffs: ", (cl==coef).all())
+        # print("  model: ", ml==clf)
+        print("Finished interpreting subject: ", sid)
 
     elif args['runtype'] == 2:
         # not yet implemented
-        # select subject
-        # read in the weights
-        # make a series of subplots (1 per nonzero node)
-        # for each node
-        # visualize the subject's node in a single subplot
+        # generate set of ROC curves
+        # use Interpolation.plotAve(xss, yss)
         k = 1
---
+++ code/plot_ave_roc.py
@@ -0,0 +1,133 @@
+'''
+Created on 18 Dec 2012
+
+@author: James McInerney
+'''
+
+#plots average roc curve for set of roc curves using linear interpolation
+from matplotlib.pyplot import plot, show, figure, xlim, ylim, title, scatter
+from numpy import array,random,arange,zeros
+from random import choice
+
+class Interpolation(object):
+    
+    def __init__(self,xs,ys):
+        assert len(xs)==len(ys)
+#        assert self._monotonically_increasing(xs)
+#        assert self._monotonically_increasing(ys)
+        xs1, ys1 = self._sort_xys(xs,ys) #throws error if xs,ys not monotonically increasing
+        self.xs, self.ys = self._remove_dups(xs1,ys1) #remove duplicate x values (taking max of corresponding y values)
+        self.min_x = self.xs[0]
+        self.max_x = self.xs[-1] #get last element in list
+    
+    def _monotonically_increasing(self,vs):
+        #returns True iff vs list is monotonically increasing (not necessarily strictly increasing)
+        i = 0
+        print 'vs',vs
+        while vs[i]<=vs[i+1] and i<(len(vs)-1): i+=1
+        return i==(len(vs)-1)
+    
+    def _sort_xys(self,xs,ys):
+        xs_is = zip(xs, range(len(xs))) #zip indices with values
+        xs1,is1 = zip(*sorted(xs_is))
+        ys1 = []
+        for i in is1: ys1.append(ys[i])
+        return xs1,ys1
+
+    def _remove_dups(self,xs,ys):
+        #removes duplicate x values (taking max of corresponding y values)
+        i = 0
+        xs1,ys1 = [], []
+        while i < len(xs):
+            #search forwards for matching x values (could be more than 2)
+            j = 1
+            while (i+j) < len(xs) and xs[i]==xs[i+j]: j+=1
+            y_max = max(ys[i:(i+j)])
+            xs1.append(xs[i])
+            ys1.append(y_max)
+            i+=j
+        return xs1,ys1
+    
+    def getY(self,x):
+        #get y by direct value or linear interpolation
+        assert x >= self.min_x and x <= self.max_x
+        #find nearest x value
+        i = 0
+        while self.xs[i]<x: i+=1 #by previous assertion, we know that in worst case self.xs[N-1]==x
+        if self.xs[i]==x:
+            #just return direct value:
+            return self.ys[i]
+        else:
+            #return linear interpolation:
+            m = (self.ys[i]-self.ys[i-1])/(self.xs[i]-self.xs[i-1])
+            return m*(x - self.xs[i-1]) + self.ys[i-1]
+    
+    def getAllY(self,xs):
+        ys = []
+        for x in xs:
+            ys.append(self.getY(x))
+        return ys
+
+class RocSet(object):
+    def __init__(self,xss,yss):
+        assert len(xss)==len(yss)
+        self.rocs = []
+        for xs,ys in zip(xss,yss):
+            self.rocs.append(Interpolation(xs,ys))
+    
+    def plot_values(self,N=100):
+        interval = 1/float(N)
+        xs = arange(0.,1.,interval)
+        ys = zeros(N)
+        assert len(xs)==len(ys)
+        for roc in self.rocs:
+            ys += array(roc.getAllY(xs))
+        ys = ys/float(len(self.rocs)) #average
+        return xs,ys
+    
+def plotAve(xss,yss):
+    #plots single roc corresponding to average of unsorted set of xs,ys (FP,TP) 
+    roc_ave = RocSet(xss,yss)
+    xs,ys = roc_ave.plot_values(N=150)
+    plot(xs,ys)
+    title('Average ROC')
+    # for xs,ys in zip(xss,yss):
+    #     #title('ROC %i'%i)
+    #     scatter(xs,ys)
+    xlim((0.,1.))
+    ylim((0.,1.))
+    i=1
+    for xs,ys in zip(xss,yss):
+        figure()
+        title('ROC %i'%i)
+        scatter(xs,ys)
+        xlim((0.,1.))
+        ylim((0.,1.))
+        i+=1
+    show()
+    
+def genRandom(size=3):
+    #generate |size| random roc curves (xs=FP, ys=TP)
+    xss,yss = [], []
+    for _ in range(size):
+        xs = [0.0,1.0]
+        xprev = 0.0
+        for _ in range(10):
+            xs.append(xprev + choice([0.0,random.beta(1,10)*(1.-xprev)])) #put in some duplicate xs, as may happen from time to time
+            xprev = xs[-1]
+        xss.append(xs)
+    for _ in range(size):
+        ys = [0.0,1.0]
+        yprev = 0.0
+        for _ in range(10):
+            ys.append(yprev + random.beta(1,5)*(1.-yprev))
+            yprev = ys[-1]
+        yss.append(ys)
+    return xss,yss
+
+def test():
+    xss,yss = genRandom()
+    plotAve(xss,yss)
+    
+if __name__ == '__main__':
+    test()
