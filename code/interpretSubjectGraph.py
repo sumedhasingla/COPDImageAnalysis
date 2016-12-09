8bb9a77a99932e4169d41d29e497a4da9b0592a0
Jenna <jmschabdach@gmail.com>
2016-12-09 10:08:08 -0500

    um lists

--- code/interpretSubjectGraph.py
+++ code/interpretSubjectGraph.py
@@ -429,7 +429,9 @@ if __name__ == '__main__':
         # generate list of ROC curves
         for i in xrange(N):
             #load the known labels (whether a node is abnormal or not)
-            yTrue = [np.zeros((patches - y[i])), np.ones((y[i]))]
+            yTrue = np.zeros((patches - y[i]))
+            if y[i] > 0.0
+                yTrue.extend(np.ones((y[i])))
             print(len(yTrue))
             # load the coefficients
             coeffFN = coefFileRoot+"S"+str(i).zfill(4)+".hdf5"
